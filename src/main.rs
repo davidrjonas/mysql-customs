4a6d931cbf06bf578abcfd4a428e2db00c42148e
David Jonas <djonas@noip.com>
2022-05-16 12:47:12 -0700

    Move table_info to its own module

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,8 @@
-use std::collections::HashMap;
 use std::fs::File;
 use std::io::Write;
 use std::path::PathBuf;
 
 use clap::Parser;
-use color_eyre::eyre::ContextCompat;
 use color_eyre::eyre::{Result, WrapErr};
 use indexmap::IndexMap;
 use mysql::prelude::*;
@@ -12,9 +10,11 @@ use serde::Deserialize;
 
 mod output;
 mod ser_mysql;
+mod table_info;
 mod transforms;
 
 use output::*;
+use table_info::*;
 use transforms::*;
 
 #[derive(Parser, Debug)]
@@ -133,80 +133,4 @@ fn process_table(
     }
 
     Ok(())
-}
-
-struct TableInfo {
-    db_name: String,
-    table_name: String,
-    columns_by_name: HashMap<String, usize>,
-    pub column_types: Vec<mysql::consts::ColumnType>,
-    pub column_names: Vec<String>,
-    row_count: usize,
-}
-
-impl TableInfo {
-    pub fn get(
-        mysql: &mysql::Pool,
-        db_name: &str,
-        table_name: &str,
-        filter: &str,
-    ) -> Result<Option<Self>> {
-        let row_count: usize = mysql
-            .get_conn()?
-            .query_first(format!(
-                "SELECT COUNT(*) FROM `{db_name}`.`{table_name}` WHERE {} LIMIT 1",
-                filter
-            ))?
-            .wrap_err_with(|| format!("failed to get count of {db_name}.{table_name}"))?;
-
-        let maybe_row: Option<mysql::Row> = mysql
-            .get_conn()?
-            .query_first(format!("SELECT * FROM `{db_name}`.`{table_name}` LIMIT 1",))?;
-        match maybe_row {
-            None => Ok(None),
-            Some(row) => Ok(Some(Self {
-                db_name: db_name.into(),
-                table_name: table_name.into(),
-                columns_by_name: Self::index_columns(&row),
-                column_types: Self::column_types(&row),
-                column_names: row
-                    .columns_ref()
-                    .iter()
-                    .map(|c| c.name_str().to_string())
-                    .collect(),
-                row_count,
-            })),
-        }
-    }
-
-    pub fn get_column_index(&self, column_name: &str) -> usize {
-        *self
-            .columns_by_name
-            .get(column_name)
-            .wrap_err_with(|| {
-                format!(
-                    "Failed to find column named {} in {}.{}. Columns: {}",
-                    column_name,
-                    self.db_name,
-                    self.table_name,
-                    serde_json::to_string(&self.column_names).expect("valid json")
-                )
-            })
-            .expect("valid column")
-    }
-
-    fn index_columns(row: &mysql::Row) -> HashMap<String, usize> {
-        let columns = row.columns_ref();
-        let mut index = HashMap::new();
-
-        for (i, c) in columns.iter().enumerate() {
-            index.insert(c.name_str().into_owned(), i);
-        }
-
-        index
-    }
-
-    fn column_types(row: &mysql::Row) -> Vec<mysql::consts::ColumnType> {
-        row.columns_ref().iter().map(|c| c.column_type()).collect()
-    }
 }
