3ed1d1cbbe43c09500ef0abfecbe048b8ec9edef
David Jonas <djonas@noip.com>
2022-05-18 15:36:51 -0700

    ser_mysql: move TIMESTAMP, TIMESTAMP2 to the string type

--- src/ser_mysql.rs
+++ src/ser_mysql.rs
@@ -65,19 +65,21 @@ where
     use mysql::consts::ColumnType::*;
 
     match column_type {
-        MYSQL_TYPE_VARCHAR
-        | MYSQL_TYPE_DATE
+        MYSQL_TYPE_DATE
         | MYSQL_TYPE_DATETIME
         | MYSQL_TYPE_DATETIME2
         | MYSQL_TYPE_ENUM
+        | MYSQL_TYPE_GEOMETRY
         | MYSQL_TYPE_JSON
         | MYSQL_TYPE_NEWDATE
         | MYSQL_TYPE_SET
+        | MYSQL_TYPE_STRING
         | MYSQL_TYPE_TIME
         | MYSQL_TYPE_TIME2
-        | MYSQL_TYPE_VAR_STRING
-        | MYSQL_TYPE_GEOMETRY
-        | MYSQL_TYPE_STRING => {
+        | MYSQL_TYPE_TIMESTAMP
+        | MYSQL_TYPE_TIMESTAMP2
+        | MYSQL_TYPE_VARCHAR
+        | MYSQL_TYPE_VAR_STRING => {
             serializer.serialize_str(std::str::from_utf8(bytes).expect("valid utf8"))
         }
         MYSQL_TYPE_LONG_BLOB | MYSQL_TYPE_MEDIUM_BLOB | MYSQL_TYPE_TINY_BLOB | MYSQL_TYPE_BLOB => {
@@ -98,14 +100,12 @@ where
                     .expect("valid decimal"),
             )
         }
-        MYSQL_TYPE_TIMESTAMP | MYSQL_TYPE_TIMESTAMP2 | MYSQL_TYPE_LONG | MYSQL_TYPE_LONGLONG => {
-            serializer.serialize_i64(
-                std::str::from_utf8(bytes)
-                    .expect("valid utf8")
-                    .parse()
-                    .expect("valid long"),
-            )
-        }
+        MYSQL_TYPE_LONG | MYSQL_TYPE_LONGLONG => serializer.serialize_i64(
+            std::str::from_utf8(bytes)
+                .expect("valid utf8")
+                .parse()
+                .expect("valid long"),
+        ),
         MYSQL_TYPE_YEAR | MYSQL_TYPE_TINY | MYSQL_TYPE_SHORT => serializer.serialize_i16(
             std::str::from_utf8(bytes)
                 .expect("valid utf8")
